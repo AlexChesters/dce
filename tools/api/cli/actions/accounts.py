eefc7c494c746ad4c10e3b1cc091d14c0a577078
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-06-25 16:07:10 +0100

    list

--- tools/api/cli/actions/accounts.py
+++ tools/api/cli/actions/accounts.py
@@ -5,12 +5,27 @@ import inquirer
 from cli.api_client.accounts import list_accounts, add_account, delete_account
 from cli.actions import Action
 
+
 class AccountsAction(Action):
     def list_accounts(self):
         return list_accounts(self.url, self.auth)
 
     def add_account(self):
-        answers = inquirer.prompt([inquirer.Text("account_id", message="What is the account ID?")])
+        sandbox_accounts_ids = [
+            "905418121097", # alpha
+            "891377354273", # bravo
+            "471112670300" # charlie
+        ]
+
+        accounts_in_pool = [account["id"] for account in self.list_accounts()]
+
+        answers = inquirer.prompt([
+            inquirer.List(
+                "account_id",
+                message="Select an account to add to the pool",
+                choices=list(set(sandbox_accounts_ids) - set(accounts_in_pool))
+            )
+        ])
         account_id = str(answers["account_id"])
 
         return add_account(self.url, self.auth, account_id)
