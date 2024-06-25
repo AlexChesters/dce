1074b1fdccb90385eb14514ce5b3558ddb034c82
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2024-06-25 10:08:19 +0100

    update script

--- tools/apitest/api_test.py
+++ tools/apitest/api_test.py
@@ -3,26 +3,10 @@ from urllib.parse import urlparse
 
 import requests
 from aws_requests_auth.boto_utils import BotoAWSRequestsAuth
-import boto3
-
-def assume_role(session, role_arn):
-    client = session.client("sts")
-
-    response = client.assume_role(
-        RoleArn=role_arn,
-        RoleSessionName="api-test"
-    )
-
-    return response["Credentials"]
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--api-url")
 args = parser.parse_args()
-
-credentials = assume_role(
-    boto3.Session(profile_name="dce-master"),
-    "arn:aws:iam::637423502760:role/dce-api-admin-role"
-)
 
 url = urlparse(args.api_url)
 
